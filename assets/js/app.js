667f2b40b8471117404c7610f7aa355261ea79e0
ArthurAmaral17 <arthuramaralzeus2@gmail.com>
2026-03-18 08:36:03 -0300

    app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// Configurações do Supabase (O Silas vai preencher aqui)
+// Configurações do Supabase ((O Silas vai preencher aqui))
 const SUPABASE_URL = 'SUA_URL_AQUI';
 const SUPABASE_KEY = 'SUA_KEY_AQUI';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
